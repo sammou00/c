dcd69da9d15584ac23c81d0643371d19851fd326
sam <sam@sam.local>
2024-05-29 16:17:09 +0200

    initial commit

---
+++ chap-1/0-exmaples/comments.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    /*
+    This is a multi-line comment
+    */
+    printf("Hello, World!\n"); // This is a comment
+    return 0;
+}
---
+++ chap-1/0-exmaples/hello-world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
+
+/*
+To run this program you need to :
+1. Compile the code using gcc  gcc hello-world.c -o hello-world
+2. Run the executable ./hello-world
+*/
